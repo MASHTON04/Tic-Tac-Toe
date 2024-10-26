c4da607b779ab0d260ea30e95152a2773a11444a
MASHTON04 <111826189+MASHTON04@users.noreply.github.com>
2024-10-25 22:50:27 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "board.hpp"
+#include "console.hpp"
+#include "game_state.hpp"
+#include "game.hpp"
+#include "human_player.hpp"
+
+
+int main()
+{
+  Board board;
+  GameState game_state(&board);
+  Console console(&board);
+  HumanPlayer player_one(&board, 'X');
+  HumanPlayer player_two(&board, 'O');
+
+  while (game_state.current_state() == "in-progress")
+  {
+    std::cout << console.display();
+    player_one.get_move();
+    player_two.get_move();
+  }
+}
